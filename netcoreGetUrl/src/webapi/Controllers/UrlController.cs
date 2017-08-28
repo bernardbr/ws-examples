9c29c510aeea1b904c208e680da3a876fd5e4677
Bernardo Esbérard <bernardo.dsn.erp@alterdata.com.br>
2017-08-28 14:38:41 -0300

    Netcore example.

---
+++ netcoreGetUrl/src/webapi/Controllers/UrlController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    public class UrlController : Controller
+    {
+        // GET api/values
+        [HttpGet("{tipo}/{area}/{subarea}")]
+        public dynamic Get(string tipo, string area, string subarea)
+        {
+            return new
+            {
+                Link = $"https://www.minhaurl.com.br/?curso={tipo}&area={area}&subarea={subarea}"
+            };
+        }
+    }
+}
